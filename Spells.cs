456282f0f3b50229c61f8280e491a7c83a3c0d2e
TheXYZ34 <mbighead5@gmail.com>
2015-11-15 22:19:34 +0200

    Update Spells.cs

--- Spells.cs
+++ Spells.cs
@@ -1,23 +1,5 @@
---// Key            SubMenu.Add("keybind", new KeyBind("Keybind", false, KeyBind.BindTypes.PressToggle, 'W')); // Here false is the default value
---// CheckBox             SubMenu.Add("checkbox", new CheckBox("CheckBox", false)); // Add(displayName, new CheckBox(inGameName, default value);
-
-// Spells HERE
-using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Menu.Values;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EloBuddy;
-using EloBuddy.SDK.Enumerations;
-
-namespace EloBuddy_Tutorial
-{
-    class Program
-    {
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
+ 
         private static Menu Menu; // Main Menu
         private static Menu SubMenu; // Sub Menu
 
@@ -26,3 +8,11 @@ namespace EloBuddy_Tutorial
         private static Spell.Active E = new Spell.Active(SpellSlot.E);
         private static Spell.Skillshot R = new Spell.Skillshot(SpellSlot.R, 600, SkillShotType.Circular, 20, int.MaxValue, 250);
         private static Spell.Targeted Ignite = null;
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
+
+
+--// Key            SubMenu.Add("keybind", new KeyBind("Keybind", false, KeyBind.BindTypes.PressToggle, 'W')); // Here false is the default value
+--// CheckBox             SubMenu.Add("checkbox", new CheckBox("CheckBox", false)); // Add(displayName, new CheckBox(inGameName, default value);
+
+
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
