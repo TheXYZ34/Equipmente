540f20c802738f5f0769515e84ca3713c96d7b16
TheXYZ34 <mbighead5@gmail.com>
2015-11-15 22:18:21 +0200

    Create OnlySpell.cs
    Just spells

---
+++ OnlySpell.cs
@@ -0,0 +1,12 @@
+// Annie 
+        private static  Spell.Targeted Q = new Spell.Targeted(SpellSlot.Q, 625);
+        private static Spell.Skillshot W = new Spell.Skillshot(SpellSlot.W, 625, SkillShotType.Cone, 250, int.MaxValue, 210);
+        private static Spell.Active E = new Spell.Active(SpellSlot.E);
+        private static Spell.Skillshot R = new Spell.Skillshot(SpellSlot.R, 600, SkillShotType.Circular, 20, int.MaxValue, 250);
+        private static Spell.Targeted Ignite = null;
+        
+        
+        
+        
+        
+//-----------------------------------------------------------------------------------------------------------------------------//
